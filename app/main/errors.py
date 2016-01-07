8a1455bb186ec5b5955e0b93118544aabceeb3a6
Troy Holsapple <troy.holsapple@gmail.com>
2016-01-06 19:05:15 -0800

    a few error redirections

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,2 +1,15 @@
 #!env/bin/python
 
+from app import app
+
+@app.errorhandler(403)
+def forbidden(error):
+  return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def not_found_error(error):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def not_found_error(error):
+  return render_template('500.html'), 500
