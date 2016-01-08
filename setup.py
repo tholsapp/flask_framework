2fa89beb250bf6465596b01b3be48aa5e9ff41ed
Troy Holsapple <troy.holsapple@gmail.com>
2016-01-08 14:41:15 -0800

    Requirents add

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+import os
+
+from setuptools import setup
+
+README = None
+with open(os.path.abspath('README.md')) as fh:
+  README = fh.read()
+
+setup (
+    name='flask_framework',
+    version='0.0',
+    description=README,
+    author='Troy Holsapple',
+    author_email='troy.holsapple@gmail.com',
+    url='http://www.flask.com',
+    packages=['flask_framework'],
+    install_requires=[
+      'Flask>=0.10.1',
+      'configobj',
+      'Flask-SQLAlchemy'
+      ]
+)
