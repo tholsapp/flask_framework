6c988e365b1daf46cdb5a7e23c86d8bfcf7a4b96
Troy Holsapple <troy.holsapple@gmail.com>
2016-01-07 19:52:32 -0800

    update

--- config/config.py
+++ config/config.py
@@ -11,17 +11,16 @@ config = ConfigObj()
 
 config.filename = 'config.py'
 
+# If set to True, Flask-SQLAlchemy will track modifications of objects and emit signals.
+# The default is None, which enables tracking but issues a warning that it will be disabled by default in the future.
+# This requires extra memory and should be disabled if not needed. #
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # the URI is required by flask-sqlalchemy extension. This is the path of the database file #
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 # the REPO is where the migration files are stored #
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
-# If set to True, Flask-SQLAlchemy will track modifications of objects and emit signals.
-# The default is None, which enables tracking but issues a warning that it will be disabled by default in the future.
-# This requires extra memory and should be disabled if not needed. #
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 """ Initialize Flask Configurations """
